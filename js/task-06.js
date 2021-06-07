bd06949cceeb130b59d06ab8356aae78dfd8a740
anton41k <anton12petrenko@gmail.com>
2021-06-07 14:34:14 +0300

    дз7 js

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ document.querySelector('#validation-input').addEventListener('blur', ev => {
   const inputEl = ev.currentTarget;
   const lenValueInput = inputEl.value.length;
   const lenValueValid = inputEl.getAttribute('data-length');
-  if (lenValueInput <= lenValueValid) {
+  if (lenValueInput === lenValueValid) {
     inputEl.classList.add('valid');
     inputEl.classList.remove('invalid');
   } else {
